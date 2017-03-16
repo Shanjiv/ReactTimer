5283ee40b80e45a06cd70cdbff3563b6f45631db
Shanseevan Kanagaratnam <shanjiv@hotmail.com>
2017-03-16 02:52:33 +0100

    Added Main and Navigation component

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            React-Timer App
+          </li>
+          <li >
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/" activeClassNAme="active-link">Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by <a href="http://www.github.com/Shanjiv" target="_blank">Shan</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Navigation;
